e3cc91d26761cc034e34724d52edd8f9e35c07fa
itCommon <1gnacio3lizondo@gmail.com>
2015-03-13 13:37:33 -0600

    PrimeraSubida

---
+++ src/primerproyecto/PrimerProyecto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyecto;
+
+/**
+ *
+ * @author Ignacio Elizondo
+ */
+public class PrimerProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Compañeros este es el primer proyecto. Cada quien cree las clases asignadas 
+        System.out.println("Proyecto para compartir");
+    }
+    
+}
